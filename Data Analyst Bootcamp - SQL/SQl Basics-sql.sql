87798d53d504f0230b7313514cf43fea00946345
Abe Diaz <74512335+FL-Marine@users.noreply.github.com>
2023-05-09 16:39:55 -0400

    Update SQl Basics-sql.sql

--- Data Analyst Bootcamp - SQL/SQl Basics-sql.sql
+++ Data Analyst Bootcamp - SQL/SQl Basics-sql.sql
@@ -1,1 +1,17 @@
 
+Insert Into EmployeeSalary VALUES
+(1001, 'Salesman', 45000),
+(1002, 'Receptionist', 36000),
+(1003, 'Salesman', 63000),
+(1004, 'Accountant', 47000),
+(1005, 'HR', 50000),
+(1006, 'Regional Manager', 65000),
+(1007, 'Supplier Relations', 41000),
+(1008, 'Salesman', 48000),
+(1009, 'Accountant', 42000)
+
+
+SELECT TOP (1000) [EmployeeID]
+      ,[JobTitle]
+      ,[Salary]
+  FROM [SQL Tutorial].[dbo].[EmployeeSalary]
