531cf18ea2bcf6474454ddf4a0027efffb0c0f72
Abe Diaz <74512335+FL-Marine@users.noreply.github.com>
2023-05-10 10:34:10 -0400

    Update SQl Basics-sql.sql

--- Data Analyst Bootcamp - SQL/SQl Basics-sql.sql
+++ Data Analyst Bootcamp - SQL/SQl Basics-sql.sql
@@ -76,3 +76,127 @@ SELECT TOP (1000) [EmployeeID]
 | 1007       | Supplier Relations | 41000  |
 | 1008       | Salesman           | 48000  |
 | 1009       | Accountant         | 42000  |
+
+ /*
+  Select Statement
+  *, Top, Distinct, Count, As, Max, Min, Avg
+  */
+
+  -- SELECT evertthing 
+  SELECT * 
+  FROM EmployeeDemographics
+
+  -- TOP 5 * 
+  SELECT TOP 5 *
+  FROM EmployeeDemographics
+
+  -- DISTINCT
+  SELECT DISTINCT(EmployeeID)
+  FROM EmployeeDemographics
+
+  -- COUNT
+  SELECT COUNT(LastName) AS LastNameCount
+  FROM EmployeeDemographics
+
+  -- MAX
+  SELECT MAX(Salary)
+  FROM EmployeeSalary
+  
+  -- MIN
+  SELECT MIN(Salary)
+  FROM EmployeeSalary
+  
+  -- AVG
+  SELECT AVG(Salary)
+  FROM EmployeeSalary
+
+  /* WHERE statement
+  =, <>, <, >, AND, OR, LIKE, NULL, NOT NULL, IN
+  */
+
+  -- =
+  SELECT * 
+  FROM EmployeeDemographics
+  WHERE FirstName = 'Jim'
+
+  -- <> Not equal
+  SELECT * 
+  FROM EmployeeDemographics
+  WHERE FirstName <> 'Jim'
+
+  -- > greater than
+  SELECT * 
+  FROM EmployeeDemographics
+  WHERE Age > 30
+
+   -- >= greater than or equal to
+  SELECT * 
+  FROM EmployeeDemographics
+  WHERE Age >= 30
+
+  -- < less than
+  SELECT * 
+  FROM EmployeeDemographics
+  WHERE Age < 32
+
+   -- <= less than or equal to
+  SELECT * 
+  FROM EmployeeDemographics
+  WHERE Age <= 32
+
+  -- <= less than or equal to AND
+  SELECT * 
+  FROM EmployeeDemographics
+  WHERE Age <= 32 AND Gender = 'Male'
+
+  -- <= less than or equal to & OR 
+  SELECT * 
+  FROM EmployeeDemographics
+  WHERE Age <= 32 OR Gender = 'Male'
+
+  -- LIKE % = wild card
+  SELECT * 
+  FROM EmployeeDemographics
+  WHERE LastName LIKE 'S%'
+
+  -- LIKE % = wild card
+  SELECT * 
+  FROM EmployeeDemographics
+  WHERE LastName LIKE '%S%'
+  -- LIKE '%S%' searching for the letter 'S' anywhere in their name
+
+  -- LIKE % = wild card
+  SELECT * 
+  FROM EmployeeDemographics
+  WHERE LastName LIKE 'S%o%'
+  -- Searching for letters 'S' & 'O'
+
+  -- NULL 
+  SELECT * 
+  FROM EmployeeDemographics
+  WHERE FirstName IS NULL
+
+  -- NOT NULL
+  SELECT * 
+  FROM EmployeeDemographics
+  WHERE FirstName IS NOT NULL
+
+  -- IN - condensed way to say equal for multiple things
+  SELECT * 
+  FROM EmployeeDemographics
+  WHERE FirstName IN ('Jim', 'Michael')
+
+  /* GROUP BY, ORDER BY
+  */
+
+  -- GROUP BY
+  SELECT Gender, COUNT(Gender) AS CountGender
+  FROM EmployeeDemographics
+  WHERE Age > 31
+  GROUP BY Gender
+  ORDER BY CountGender DESC
+
+  -- ORDER BY
+  SELECT * 
+  FROM EmployeeDemographics
+  ORDER BY Age DESC, Gender DESC
