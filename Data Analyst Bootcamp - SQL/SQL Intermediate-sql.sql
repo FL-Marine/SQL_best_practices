efb6bb3f6e0e7dfa9fa3aeb5d8c6da562aa39b18
Abe Diaz <74512335+FL-Marine@users.noreply.github.com>
2023-05-11 18:35:44 -0400

    Update SQL Intermediate-sql.sql

--- Data Analyst Bootcamp - SQL/SQL Intermediate-sql.sql
+++ Data Analyst Bootcamp - SQL/SQL Intermediate-sql.sql
@@ -1,3 +1,69 @@
-select
-from 
-where
+
+  /* INNER, FULL, LEFT, RIGHT, OUTER JOINS
+  */
+
+  SELECT *
+  FROM EmployeeDemographics
+
+  SELECT *
+  FROM EmployeeSalary
+
+  -- INNER JOIN
+SELECT *
+FROM SQLTutorial.dbo.EmployeeDemographics
+INNER JOIN SQLTutorial.dbo.EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+
+-- FULL OUTER
+SELECT *
+FROM SQLTutorial.dbo.EmployeeDemographics
+FULL OUTER JOIN SQLTutorial.dbo.EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+
+-- LEFT	OUTER JOIN
+SELECT *
+FROM SQLTutorial.dbo.EmployeeDemographics
+LEFT OUTER JOIN SQLTutorial.dbo.EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+
+-- RIGHT OUTER JOIN
+SELECT *
+FROM SQLTutorial.dbo.EmployeeDemographics
+RIGHT OUTER JOIN SQLTutorial.dbo.EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+
+SELECT
+	EmployeeDemographics.EmployeeID,
+	FirstName,
+	LastName,
+	Salary
+FROM SQLTutorial.dbo.EmployeeDemographics
+INNER JOIN SQLTutorial.dbo.EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+WHERE FirstName <> 'Michael'
+ORDER BY Salary DESC
+
+SELECT JobTitle, AVG(Salary) AS AvgSalary
+FROM SQLTutorial.dbo.EmployeeDemographics
+INNER JOIN SQLTutorial.dbo.EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+WHERE JobTitle = 'Salesman'
+GROUP BY JobTitle
+
+/* UNION, UNION ALL
+*/
+
+SELECT *
+FROM SQLTutorial.dbo.EmployeeDemographics
+UNION
+SELECT * 
+FROM SQLTutorial.dbo.WareHouseEmployeeDemographics
+
+SELECT *
+FROM SQLTutorial.dbo.EmployeeDemographics
+UNION ALL
+SELECT * 
+FROM SQLTutorial.dbo.WareHouseEmployeeDemographics
+ORDER BY Employeeid
+
+
