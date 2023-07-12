63ce81da88d68a472271acb577a920f3a7caaac6
Abe Diaz <74512335+FL-Marine@users.noreply.github.com>
2023-07-12 13:54:13 -0500

    Update easy.sql

--- stratascratch - questions_solutions/easy.sql
+++ stratascratch - questions_solutions/easy.sql
@@ -13,3 +13,31 @@ LEFT JOIN db_dept AS dept
 WHERE dept.department IN ('marketing', 'engineering');
 
 -- output is 2400
+
+/* We have a table with employees and their salaries, however, some of the records are old and contain outdated salary information. 
+Find the current salary of each employee assuming that salaries increase each year. Output their id, first name, last name, department ID, and current salary. 
+Order your list by employee ID in ascending order.
+
+Hints 
+Ideally this dataset should consists of unique records of employees for only current year
+There are multiple years of data can be found for some employees
+Until now at every compensation revision cycle, all employees have received a salary increase so you can assume that the highest salary is the employee's current salary. Use a max() function to find the highest salary for each employee.
+The output should be all the details of all the employees with correct salary */
+
+select 
+    distinct id,
+    first_name,
+    last_name,
+    MAX(salary) as salary,
+    department_id
+from ms_employee_salary
+group by 
+     id,
+    first_name,
+    last_name,
+    department_id;
+| id | first_name | last_name | salary | department_id |
+| -- | ---------- | --------- | ------ | ------------- |
+| 1  | Todd       | Wilson    | 110000 | 1006          |
+| 2  | Justin     | Simon     | 130000 | 1005          |
+| 3  | Kelly      | Rosario   | 42689  | 1002          |
