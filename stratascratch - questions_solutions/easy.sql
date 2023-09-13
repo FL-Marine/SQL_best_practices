ff9925548d3c9b77fb3763cf0efb1b1a7a7dd959
Abe Diaz <74512335+FL-Marine@users.noreply.github.com>
2023-09-13 14:45:59 -0500

    Update easy.sql

--- stratascratch - questions_solutions/easy.sql
+++ stratascratch - questions_solutions/easy.sql
@@ -344,3 +344,20 @@ GROUP BY city, property_type;
 | Boston  | Apartment     | 1     | 1 |
 | Chicago | Apartment     | 2     | 2 |
 | LA      | Apartment     | 1.143 | 1.143 |
+
+/* You have been asked to find the 5 most lucrative products in terms of total revenue for the first half of 2022 (from January to June inclusive).
+Output their IDs and the total revenue.*/
+SELECT 
+    TOP 5 product_id,
+    SUM(cost_in_dollars * units_sold) AS revenue
+FROM online_orders
+WHERE date BETWEEN '2022-01-01' AND '2022-06-30'
+GROUP BY product_id
+ORDER BY revenue DESC;
+| product_id | revenue |
+| ---------- | ------- |
+| 2          | 207     |
+| 3          | 201     |
+| 5          | 199     |
+| 1          | 65      |
+| 6          | 56      |
